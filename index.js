cb20d6963acc9ef02aba9d67393cdbe78f569e36
Staffan Småros <staffan.smaros@gmail.com>
2022-08-10 13:47:59 +0300

    Completed 3.14 phonebook database step 2

--- index.js
+++ index.js
@@ -104,15 +104,15 @@ app.post('/api/persons/', (request, response) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: generateId()
-    }
+        number: body.number
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
 })
 
 const PORT = process.env.PORT || 3001
